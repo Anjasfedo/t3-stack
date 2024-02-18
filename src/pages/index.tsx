c6bfae52709568345558e6092fd23c3ebed78766
Anjasfedo <fedoaf98@gmail.com>
2024-02-18 15:41:21 +0700

    Dynamic Route of Post & Profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 
-import { SignInButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -11,6 +11,7 @@ import Image from "next/image";
 import LoadSpinner from "~/components/LoadSpinner";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -38,7 +39,7 @@ const CreatePostWizard = () => {
   });
 
   const PostDataHandler = () => {
-    mutate({ userName: user.username ?? "User", content: input });
+    mutate({ userName: user?.username ?? "User", content: input });
   };
 
   if (!user) return null;
@@ -114,8 +115,12 @@ const PostItem = ({ post, author }: PostWithUserT) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span>{`${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{`${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-xl">{post.content}</span>
       </div>
@@ -139,7 +144,7 @@ const Posts = () => {
   );
 };
 
-export default function Home() {
+export default function PostsPage() {
   const { isLoaded: userIsLoaded, isSignedIn } = useUser();
 
   // Start fetch (use the cache)
@@ -184,9 +189,27 @@ export default function Home() {
             {!!isSignedIn && (
               <>
                 <CreatePostWizard />
-                {/* <SignOutButton>
-                  <button>Sign out with Clerk</button>
-                </SignOutButton> */}
+                <SignOutButton>
+                  <button
+                    type="button"
+                    className="mb-2 me-2 ml-3 inline-flex items-center rounded-lg bg-[#24292F] px-2 py-1 text-center text-sm font-medium text-white hover:bg-[#24292F]/90 focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 dark:hover:bg-[#050708]/30 dark:focus:ring-gray-500"
+                  >
+                    <svg
+                      className="me-2 h-4 w-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    Sign out
+                  </button>
+                </SignOutButton>
               </>
             )}
           </div>
