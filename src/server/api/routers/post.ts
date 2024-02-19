50704e98229ade447c5557534c63158862c50bcd
Anjasfedo <fedoaf98@gmail.com>
2024-02-19 22:32:46 +0700

    Create profileRouter

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,4 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
@@ -9,16 +8,10 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
+import { filterUserClient } from "~/server/helpers/filterUserClient";
+
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
-
-const filterUserClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
